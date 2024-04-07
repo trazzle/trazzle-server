0ed02c0d0d443cdcc6e3894bc3e8056fbfcd754f
DongSeong Seo <sds901234@naver.com>
2024-04-08 00:23:59 +0900

    :sparkles: Update: 마그넷 리뷰 반영

--- src/modules/magnets/controller/magnets.controller.ts
+++ src/modules/magnets/controller/magnets.controller.ts
@@ -1,35 +1,7 @@
-import {
-  BadRequestException,
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-  Put,
-  Query,
-  UploadedFiles,
-  UseGuards,
-  UseInterceptors,
-} from "@nestjs/common";
+import { Controller, Get, ParseIntPipe, Query, UseGuards } from "@nestjs/common";
 import { MagnetsService } from "../service/magnets.service";
-import { CreateMagnetDto } from "src/modules/magnets/dtos/create-magnet.dto";
-import {
-  ApiBearerAuth,
-  ApiBody,
-  ApiConsumes,
-  ApiOkResponse,
-  ApiOperation,
-  ApiParam,
-  ApiQuery,
-  ApiTags,
-} from "@nestjs/swagger";
+import { ApiBearerAuth, ApiConsumes, ApiOperation, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "src/guards/jwt-auth.guard";
-import { MagnetEntity } from "src/modules/magnets/entities/magnet.entity";
-import { FileFieldsInterceptor } from "@nestjs/platform-express";
-import { SignInUser } from "src/decorators/sign-in-user.decorator";
-import { UserEntity } from "src/modules/users/entities/user.entity";
 
 @ApiBearerAuth()
 @ApiTags("마그넷")
@@ -37,37 +9,6 @@ import { UserEntity } from "src/modules/users/entities/user.entity";
 @Controller()
 export class MagnetsController {
   constructor(private readonly magnetsService: MagnetsService) {}
-
-  @ApiConsumes("multipart/form-data")
-  @ApiOperation({ summary: "마그넷 생성" })
-  @ApiOkResponse({ description: "마그넷", type: MagnetEntity })
-  @ApiBody({
-    schema: {
-      type: "object",
-      properties: {
-        image: {
-          type: "file",
-          format: "binary",
-          description: "마그넷 이미지",
-        },
-        cityId: { type: "number", example: "1", description: "도시 ID" },
-      },
-      required: ["image", "cityId"],
-    },
-  })
-  @Post()
-  @UseInterceptors(FileFieldsInterceptor([{ name: "image", maxCount: 1 }]))
-  create(
-    @SignInUser() user: UserEntity,
-    @Body() body: CreateMagnetDto,
-    @UploadedFiles() { image }: { image: Express.Multer.File[] },
-  ) {
-    if (!image) {
-      throw new BadRequestException("이미지가 필요합니다.");
-    }
-
-    return this.magnetsService.create(user.id, body, image[0]);
-  }
 
   @ApiConsumes("application/json")
   @ApiOperation({ summary: "마그넷 조회" })
@@ -81,44 +22,5 @@ export class MagnetsController {
   @Get()
   findAll(@Query("cityId", ParseIntPipe) cityId: number) {
     return this.magnetsService.list(cityId);
-  }
-
-  @ApiConsumes("multipart/form-data")
-  @ApiOperation({ summary: "마그넷 수정" })
-  @ApiBody({
-    schema: {
-      type: "object",
-      properties: {
-        image: {
-          type: "file",
-          format: "binary",
-          description: "마그넷 이미지",
-        },
-      },
-      required: ["image"],
-    },
-  })
-  @Put(":id")
-  @UseInterceptors(FileFieldsInterceptor([{ name: "image", maxCount: 1 }]))
-  update(@Param("id", ParseIntPipe) id: number, @UploadedFiles() { image }: { image: Express.Multer.File[] }) {
-    if (!image) {
-      throw new BadRequestException("이미지가 필요합니다.");
-    }
-
-    return this.magnetsService.update(id, image[0]);
-  }
-
-  @ApiConsumes("application/json")
-  @ApiOperation({ summary: "마그넷 삭제" })
-  @ApiParam({
-    name: "id",
-    required: true,
-    type: "number",
-    description: "마그넷 고유번호 PK",
-    example: 1,
-  })
-  @Delete(":id")
-  delete(@Param("id", ParseIntPipe) id: number) {
-    return this.magnetsService.delete(id);
   }
 }
--- src/modules/magnets/service/magnets.service.ts
+++ src/modules/magnets/service/magnets.service.ts
@@ -1,10 +1,6 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { CreateMagnetDto } from "src/modules/magnets/dtos/create-magnet.dto";
+import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/modules/core/database/prisma/prisma.service";
-import { putObjectCommandDto } from "src/modules/core/aws-s3/dtos/s3-command.dto";
 import { AwsS3Service } from "src/modules/core/aws-s3/aws-s3.service";
-import { CitiesService } from "src/modules/cities/service/cities.service";
-import { v4 as uuidv4 } from "uuid";
 import { Prisma } from "@prisma/client";
 
 const select: Prisma.MagnetSelect = {
@@ -17,93 +13,12 @@ export class MagnetsService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly awsS3Service: AwsS3Service,
-    private readonly citiesService: CitiesService,
   ) {}
-
-  create(id: number, body: CreateMagnetDto, image: Express.Multer.File) {
-    return this.prismaService.$transaction(async transaction => {
-      const city = await transaction.city.findUnique({ where: { id: body.cityId } });
-      console.log(city);
-      if (!city) {
-        throw new NotFoundException("도시를 찾을 수 없습니다.");
-      }
-
-      const request: putObjectCommandDto = {
-        Key: `magnets/${city.name}/${uuidv4()}`,
-        Body: image.buffer,
-        ContentType: image.mimetype,
-      };
-      const { url, Key } = await this.awsS3Service.uploadImageToS3Bucket(request);
-
-      const magnet = await transaction.magnet.create({
-        data: {
-          url: url,
-          // key: Key,
-          cityId: body.cityId,
-        },
-      });
-
-      return magnet;
-    });
-  }
 
   list(cityId: number) {
     return this.prismaService.magnet.findMany({
-      where: {
-        cityId,
-      },
+      where: { cityId },
       select,
-    });
-  }
-
-  update(id: number, image: Express.Multer.File) {
-    return this.prismaService.$transaction(async transaction => {
-      const magnet = await transaction.magnet.findUnique({ where: { id } });
-      if (!magnet) {
-        throw new NotFoundException("마그넷을 찾을 수 없습니다.");
-      }
-
-      // 이미지 삭제
-      // await this.awsS3Service.deleteImageFromS3Bucket({
-      //   Key: magnet.key,
-      // });
-      await this.awsS3Service.delete(magnet.url);
-
-      const city = await transaction.city.findUnique({ where: { id: magnet.cityId } });
-
-      const request: putObjectCommandDto = {
-        Key: `magnets/${city.name}/${uuidv4()}`,
-        Body: image.buffer,
-        ContentType: image.mimetype,
-      };
-      const { url, Key } = await this.awsS3Service.uploadImageToS3Bucket(request);
-
-      await transaction.magnet.update({
-        where: { id },
-        data: {
-          url: url,
-          // key: Key,
-        },
-      });
-    });
-  }
-
-  delete(id: number) {
-    return this.prismaService.$transaction(async transaction => {
-      const magnet = await transaction.magnet.findUnique({ where: { id } });
-
-      if (!magnet) {
-        throw new NotFoundException("마그넷을 찾을 수 없습니다.");
-      }
-
-      await this.awsS3Service.delete(magnet.url);
-      // await this.awsS3Service.deleteImageFromS3Bucket({
-      //   Key: magnet.key,
-      // });
-
-      await transaction.magnet.delete({
-        where: { id },
-      });
     });
   }
 }
