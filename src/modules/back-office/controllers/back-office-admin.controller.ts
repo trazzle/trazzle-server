d83d4277e29bb9c684cf5ef5a84e28c1d90b9fae
eunkang-dyphi <eunkang@dyphi.com>
2024-04-23 14:13:01 +0900

    :sparkles: Init: 백오피스 내부컨트롤러/서비스 API 초기셋팅

--- src/modules/back-office/controllers/back-office-admin.controller.ts
+++ src/modules/back-office/controllers/back-office-admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { BackOfficeAdminService } from "src/modules/back-office/services/back-office-admin.service";
 
@@ -9,5 +9,31 @@ export class BackOfficeAdminController {
 
   @ApiOperation({ summary: "신규 관리자 회원 생성" })
   @Post()
-  async createAdminUser() {}
+  async createAdmin() {
+    return "createAdmin";
+  }
+
+  @ApiOperation({ summary: "신규 관리자 회원 목록 조회" })
+  @Get()
+  async getAdmins() {
+    return "getAdmins";
+  }
+
+  @ApiOperation({ summary: "신규 관리자 회원 단건 조회" })
+  @Get(":id")
+  async getAdminInfo(@Param("id", ParseIntPipe) id: number) {
+    return "getAdminInfo" + id;
+  }
+
+  @ApiOperation({ summary: "관리자 회원 정보 수정" })
+  @Patch()
+  async updateAdminInfo() {
+    return "updateAdminInfo";
+  }
+
+  @ApiOperation({ summary: "관리자 회원 탈퇴" })
+  @Delete()
+  async deleteAdmin() {
+    return "deleteAdmin";
+  }
 }
--- src/modules/back-office/controllers/back-office-magnet.controller.ts
+++ src/modules/back-office/controllers/back-office-magnet.controller.ts
@@ -1,9 +1,27 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Controller, Delete, Patch, Post } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { BackOfficeMagnetService } from "src/modules/back-office/services/back-office-magnet.service";
 
 @Controller("magnets")
 @ApiTags("백오피스 API - 마그넷")
 export class BackOfficeMagnetController {
   constructor(private readonly backOfficeMagnetService: BackOfficeMagnetService) {}
+
+  @ApiOperation({ summary: "마그넷 생성" })
+  @Post()
+  async createMagnet() {
+    return "createMagnet";
+  }
+
+  @ApiOperation({ summary: "마그넷 정보 수정" })
+  @Patch()
+  async updateMagnet() {
+    return "updateMagnet";
+  }
+
+  @ApiOperation({ summary: "마그넷 삭제" })
+  @Delete()
+  async deleteMagnet() {
+    return "deleteMagnet";
+  }
 }
