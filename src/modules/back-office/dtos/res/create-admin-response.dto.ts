664ae3ef497c535c110c729d8631cfb63795693f
loveAlakazam <dmsrkd1216@gmail.com>
2024-04-25 00:35:49 +0900

    :sparkles: Add: 관리자계정추가 API 생성

---
+++ src/modules/back-office/dtos/res/create-admin-response.dto.ts
@@ -0,0 +1,23 @@
+import { OmitType } from "@nestjs/mapped-types";
+import { ApiProperty } from "@nestjs/swagger";
+import { UserEntity } from "src/modules/users/entities/user.entity";
+
+export class CreateAdminResponseDto extends OmitType(UserEntity, ["intro", "profileImageURL", "isAdmin"] as const) {
+  @ApiProperty({
+    description: "유저 PK",
+    example: 1,
+  })
+  id: number;
+
+  @ApiProperty({
+    description: "관리자 이름",
+    example: "최은강",
+  })
+  name: string;
+
+  @ApiProperty({
+    description: "관리자 계정",
+    example: "admin-{관리자계정값}",
+  })
+  account: string;
+}
--- src/modules/back-office/services/back-office-admin.service.ts
+++ src/modules/back-office/services/back-office-admin.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/modules/core/database/prisma/prisma.service";
+import { CreateAdminRequestBodyDto } from "../dtos/req/create-admin-request-body.dto";
+import { CreateAdminResponseDto } from "../dtos/res/create-admin-response.dto";
 
 @Injectable()
 export class BackOfficeAdminService {
   constructor(private prismaService: PrismaService) {}
+
+  createAdmin(dto: CreateAdminRequestBodyDto) {
+    const { name, account } = dto;
+
+    this.prismaService.$transaction(async transaction => {
+      const newAdminUser = await transaction.user.create({
+        data: {
+          name: name,
+          account: `admin-${account}`,
+          isAdmin: true,
+        },
+      });
+
+      return await transaction.user.findFirst({
+        where: {
+          id: newAdminUser.id,
+        },
+        select: {
+          id: true,
+          name: true,
+          account: true,
+        },
+      });
+    });
+  }
 }
