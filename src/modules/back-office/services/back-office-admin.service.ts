af4b054cb59af52baa09b1c4719889bb8e7b203c
loveAlakazam <dmsrkd1216@gmail.com>
2024-04-25 18:03:35 +0900

    :lock: Fix: 관리자계정 account가 중복이면 Conflict 예외처리하도록 내부로직 수정

--- src/modules/back-office/services/back-office-admin.service.ts
+++ src/modules/back-office/services/back-office-admin.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/modules/core/database/prisma/prisma.service";
 import { CreateAdminRequestBodyDto } from "../dtos/req/create-admin-request-body.dto";
 
@@ -10,6 +10,21 @@ export class BackOfficeAdminService {
     const { name, account } = dto;
 
     this.prismaService.$transaction(async transaction => {
+      // account가 중복되는지 확인
+      const user = await transaction.user.findFirst({
+        where: {
+          account: `admin-${account}`,
+        },
+        select: {
+          id: true,
+          name: true,
+          account: true,
+        },
+      });
+      if (user) {
+        throw new ConflictException("이미 존재하는 관리자 유저입니다.");
+      }
+
       const newAdminUser = await transaction.user.create({
         data: {
           name: name,
