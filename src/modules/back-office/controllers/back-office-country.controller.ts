42aa564e075b17737bf5f6948e46f878a495eacf
loveAlakazam <dmsrkd1216@gmail.com>
2024-05-02 21:09:53 +0900

    ✨ Add: (백오피스) 국가API 정상응답스웨거 데코레이터 추가

--- src/modules/back-office/controllers/back-office-country.controller.ts
+++ src/modules/back-office/controllers/back-office-country.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Param, Patch, Post, UseGuards } from "@nestjs/common";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { AdminBearerAuth } from "src/decorators/admin-auth.decorator";
 import { AdminGuard } from "src/guards/admin-auth.guard";
 import { BackOfficeCountryService } from "src/modules/back-office/services/back-office-country.service";
 import { CreateCountryDto } from "src/modules/conuntries/dtos/create-country.dto";
 
 import { UpdateCountryDto } from "src/modules/conuntries/dtos/update-country.dto";
+import { CountryEntity } from "src/modules/conuntries/entities/country.entity";
 
 @AdminBearerAuth(AdminGuard)
 @UseGuards(AdminGuard)
@@ -15,12 +16,14 @@ export class BackOfficeCountryController {
   constructor(private readonly backOfficeCountryService: BackOfficeCountryService) {}
 
   @ApiOperation({ summary: "국가 생성" })
+  @ApiOkResponse({ description: "신규 국가", type: CountryEntity })
   @Post()
   createNewCountry(@Body() dto: CreateCountryDto) {
     return this.backOfficeCountryService.create(dto);
   }
 
   @ApiOperation({ summary: "국가 정보 수정" })
+  @ApiOkResponse({ description: "수정된 국가 정보", type: CountryEntity })
   @Patch(":code")
   updateCountryInfo(@Param("code") code: string, @Body() dto: UpdateCountryDto) {
     return this.backOfficeCountryService.update(code, dto);
