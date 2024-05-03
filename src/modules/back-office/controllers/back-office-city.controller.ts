677ce66a1f235f6deddbb5684c3bbee2abe7d4fa
loveAlakazam <dmsrkd1216@gmail.com>
2024-05-03 21:10:10 +0900

    ✨ Add: (백오피스) 도시API 정상응답스웨거 데코레이터 추가

--- src/modules/back-office/controllers/back-office-city.controller.ts
+++ src/modules/back-office/controllers/back-office-city.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Param, ParseIntPipe, Patch, Post, UseGuards } from "@nestjs/common";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { AdminBearerAuth } from "src/decorators/admin-auth.decorator";
 import { AdminGuard } from "src/guards/admin-auth.guard";
 import { BackOfficeCityService } from "src/modules/back-office/services/back-office-city.service";
 import { CreateCityDto } from "src/modules/cities/dto/create-city.dto";
 import { UpdateCityDto } from "src/modules/cities/dto/update-city.dto";
+import { CityEntity } from "src/modules/cities/entities/city.entity";
 
 @AdminBearerAuth(AdminGuard)
 @UseGuards(AdminGuard)
@@ -14,12 +15,14 @@ export class BackOfficeCityController {
   constructor(private readonly backOfficeCityService: BackOfficeCityService) {}
 
   @ApiOperation({ summary: "도시 생성" })
+  @ApiOkResponse({ description: "신규 도시", type: CityEntity })
   @Post()
   createCity(@Body() createCityDto: CreateCityDto) {
     return this.backOfficeCityService.create(createCityDto);
   }
 
   @ApiOperation({ summary: "도시 정보 수정" })
+  @ApiOkResponse({ description: "수정된 도시정보", type: CityEntity })
   @Patch()
   updateCityInfo(@Param("id", ParseIntPipe) id: string, @Body() updateCityDto: UpdateCityDto) {
     return this.backOfficeCityService.update(+id, updateCityDto);
