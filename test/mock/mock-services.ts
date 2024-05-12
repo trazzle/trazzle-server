46c1f2890cefd08414984f64950b7af762cb9865
loveAlakazam <dmsrkd1216@gmail.com>
2024-05-13 01:34:53 +0900

    :heavy_plus_sign: Add: mocking 의존성 추가

--- test/mock/mock-services.ts
+++ test/mock/mock-services.ts
@@ -1,7 +1,14 @@
 import { MockClassType } from "test/mock/mock.type";
 import { BackOfficeAdminService } from "src/modules/back-office/services/back-office-admin.service";
+import { CustomConfigService } from "src/modules/core/config/custom-config.service";
+import { JwtService } from "@nestjs/jwt";
+import { RedisService } from "src/modules/core/redis/redis.service";
 
 export const mockConfigService = {
+  get: jest.fn(),
+};
+
+export const mockCustomConfigService: MockClassType<CustomConfigService> = {
   get: jest.fn(),
 };
 
@@ -11,4 +18,19 @@ export const mockBackOfficeAdminService: MockClassType<BackOfficeAdminService> =
   getAdminInfo: jest.fn(),
   updateAdminInfo: jest.fn(),
   deleteAdmin: jest.fn(),
+};
+
+export const mockJwtService: MockClassType<JwtService> = {
+  sign: jest.fn(),
+  signAsync: jest.fn(),
+  verify: jest.fn(),
+  verifyAsync: jest.fn(),
+  decode: jest.fn(),
+};
+
+export const mockRedisService: MockClassType<RedisService> = {
+  get: jest.fn(),
+  set: jest.fn(),
+  del: jest.fn(),
+  onModuleDestroy: jest.fn(),
 };
